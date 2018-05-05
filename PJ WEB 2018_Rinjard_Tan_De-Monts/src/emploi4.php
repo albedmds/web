3b5e6caea39a232075b2f37569fd08cba8c431dd
Skyfire27 <yann.rinjard@edu.ece.fr>
2018-05-05 23:06:52 +0200

    Modif

--- PJ WEB 2018_Rinjard_Tan_De-Monts/src/emploi4.php
+++ PJ WEB 2018_Rinjard_Tan_De-Monts/src/emploi4.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
 $Offre = isset($_POST["ofj"])? $_POST["ofj"]: "";
-$connect=$_SESSION['ID_Ent'];
+//$connect=$_SESSION['ID_Ent'];
 $database = "prjet_web";
 $db_handle = mysqli_connect('localhost','root','');
 $db_found = mysqli_select_db($db_handle, $database);
 if ($db_found) {
-	$sql = "INSERT INTO `offre_emploi`(`Intitule`, `Id_Entreprise`) VALUES ('$Offre','$connect')";
-	$result = mysqli_query($db_handle, $sql);
-	echo "Offre Ajouté";
+    $sql = "INSERT INTO offre_emploi(Intitule, Id_Entreprise) VALUES ('$Offre','$connect')";
+    $result = mysqli_query($db_handle, $sql);
+    echo "<script>
+alert('Offre Ajouté');
+window.location.href='emploi.html';
+</script>";
 }
 else {
-	echo "Connexion impossible.";
+    echo "Connexion impossible.";
 }
 mysqli_close($db_handle);
 ?>
