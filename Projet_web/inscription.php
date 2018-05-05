8270b6fcfc22085be54d3fffa9effca152c2333e
Skyfire27 <yann.rinjard@edu.ece.fr>
2018-05-05 15:24:20 +0200

    test mdp

--- Projet_web/inscription.php
+++ Projet_web/inscription.php
@@ -4,14 +4,19 @@ $Prenom = isset($_POST["Prenom"])? $_POST["Prenom"]: "";
 $Pseudo = isset($_POST["Pseudo"])? $_POST["Pseudo"]: "";
 $Email = isset($_POST["Email"])? $_POST["Email"]: "";
 $Mot_de_Passe = isset($_POST["Mdp"])? $_POST["Mdp"]: "";
+$CMot_de_Passe = isset($_POST["CMdp"])? $_POST["CMdp"]: "";
 $database = "prjet_web";
 $db_handle = mysqli_connect('localhost','root','');
 $db_found = mysqli_select_db($db_handle, $database);
 if ($db_found) {
+if($Mot_de_Passe==$CMot_de_Passe){
 $sql = "INSERT INTO `personne`( `Nom`, `Prenom`, `Pseudo`, `Email`, `Mot_de_Passe`) VALUES ('$Nom', '$Prenom', '$Pseudo','$Email','$Mot_de_Passe')";
 $result = mysqli_query($db_handle, $sql);
 echo "Donnée rentrée.";
-header ('Location: inscription_uti.php');
+}
+else{
+	echo "Mot de Passe différent";
+}
 }
 else {
 echo "Donnée non rentrée.";
